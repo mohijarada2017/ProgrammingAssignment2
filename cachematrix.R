c3e3f5712fa75a16327054d99e8e473f4181ac8e
Mohi Jarada <jarada.mohee@gmail.com>
2017-10-27 17:29:07 +0200

    Mohee Jarada is submitting Week#3 of R course at John Hopkins University based on this requested assignment: https://www.coursera.org/learn/r-programming/peer/tNy8H/programming-assignment-2-lexical-scoping
    Mohee Jarada is submitting Week#3 of R course at John Hopkins University based on this requested assignment: 
    https://www.coursera.org/learn/r-programming/peer/tNy8H/programming-assignment-2-lexical-scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Done by Mohee Jarada on Oct 26, 2017:
 
-## Write a short comment describing this function
+# To read about Matrix inverse, good materials to start with:
+# https://www.mathsisfun.com/algebra/matrix-inverse.html
+# https://www.statmethods.net/advstats/matrix.html
 
-makeCacheMatrix <- function(x = matrix()) {
+# This function creates a special "matrix" object that can cache its inverse:
+#  assume that the matrix supplied is always invertible
+makeCacheMatrix <- function(pMatrixData = matrix()) {
+  # Inverse matrix global env reference to store for caching:
+  matInversRef <- NULL
+  
+  set <- function(y) {
+    pMatrixData <<- y
+    matInversRef <<- NULL
+  }
+  
+  get <- function() pMatrixData
+  
+  setInverseMat <- function(solve) matInversRef <<- solve
+  getInverseMat <- function() matInversRef
+  
+  # return a list of 4 functions like in OOP style:
+  list(set = set, get = get,
+       setInverseMat = setInverseMat,
+       getInverseMat = getInverseMat)
+} # end makeCacheMatrix()
 
-}
+# /////////////////////////////////////////////////////////////////
 
-
-## Write a short comment describing this function
-
+# Below function computes the inverse of the special "matrix" returned by 
+# makeCacheMatrix() above. If the inverse has already been calculated 
+# (and the matrix has not changed), then the cachesolve should retrieve the inverse 
+# from the cache (global environment)
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  matInversRef <- x$getInverseMat()
+  
+  # Check if the inverse matrix has already been calculated and 
+  # the matrix size has not changed:
+  if(!is.null(matInversRef) & length(x$getInverseMat()) == length(matInversRef)) {
+    message("getting cached data...")
+    return(matInversRef)
+  }
+  
+  data <- x$get()
+  matInversRef <- solve(data, ...)
+  x$setInverseMat(matInversRef)
+  
+  matInversRef
+} # end cacheSolve()
+
+# 1st case: Testing it with 2x2 identity matrix and check inverse resulted cached matrix:
+retMat <- makeCacheMatrix(matrix(c(4, 2, 7, 6), 2, 2))
+cacheSolve(retMat)
+cacheSolve(retMat)
+print("###################################################x")
+# 2nd case: Testing it with 2x2 identity matrix and then change its size to be 3x3
+retMat <- NULL
+retMat <- makeCacheMatrix(matrix(c(4, 2, 7, 6), 2, 2))
+cacheSolve(retMat)
+retMat <- makeCacheMatrix(matrix(c(4, 2, 7, 6, 1, 5, 8, 10, 3), 3, 3))
+cacheSolve(retMat)
